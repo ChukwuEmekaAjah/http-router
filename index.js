09729b0ab1b74eb72a17697fe45693ef96e5f563
Chukwuemekaajah <talk2ajah@gmail.com>
2021-01-30 01:37:04 +0100

    Enable reading of static files on server

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const util = require('util');
 const qs = require('querystring');
 const url = require('url');
+const fs = require('fs');
+const path = require('path');
 
 const superHandlers = {};
+const config = {}
+function Use(Utility){
+    if(Utility.type === 'router'){
+        for(let method in Utility.handlers){
+            superHandlers[method] = Utility.handlers[method];
+        }
+    }
 
-function Use(Router){
-    for(let method in Router.handlers){
-        superHandlers[method] = Router.handlers[method];
+    if(Utility.type === 'static'){
+        config.static = Utility.setup();
+        console.log("static config is", config)
     }
 }
 
@@ -26,6 +35,16 @@ function removeFalsePositives(array){
 }
 
 function Serve(req, res){
+
+    // check for static file
+    if(isStaticFile(req.url)){
+        if(isValidStaticFile(req.url)){
+            return sendFile(req.url, res);
+        } else {
+            return res.end(`${req.url} Not found`)
+        } 
+    } 
+
     req.params = {}
     req.query = qs.parse(req.url.slice(req.url.indexOf('?')+1,));
 
@@ -67,6 +86,8 @@ function Serve(req, res){
         }
     }
     
+    
+
     return res.end(`${req.url} Not found`)
 }
 
@@ -81,11 +102,58 @@ function Router(){
             }
         },
         handlers : {},
+        type: 'router',
+    }
+}
+
+function isStaticFile(url){
+    console.log("extension name", path.extname(url))
+    return path.extname(url);
+}
+
+function isValidStaticFile(url){
+    let isValid = true;
+    try{
+        fs.accessSync(`${config.static}${url.split('/').join(path.sep)}`, fs.constants.R_OK)
+    } catch(exc){
+        console.log("file is not accessible", exc)
+        return false;
+    }
+    
+    return isValid;
+}
+
+function sendFile(url, res){
+    const fileAddress = `${config.static}${url.split('/').join(path.sep)}`;
+
+    fs.readFile(fileAddress, function(err, fileData){
+        if(err){
+            return res.end("Error reading file")
+        }
+        return res.end(fileData);
+    })
+}
+
+function Static(publicFolder){
+    let staticDirectory = publicFolder;
+    return {
+        setup: function(){
+            // let folder = null;
+            // const pathname = `${process.cwd()}${path.sep}${publicFolder}`
+            // try{
+            //     folder = fs.opendirSync(pathname);
+            // } catch(exc){
+            //     console.log("error opening static files folder", exc);
+            // }
+            return `${process.cwd()}${path.sep}${staticDirectory}`;
+        },
+        type: 'static',
     }
 }
 
 module.exports = {
     Router: Router,
     Serve: Serve,
-    Use: Use 
+    Use: Use,
+    Static: Static,
 }
